943325f471c0f23de138d44dbcdac817dda7f7d0
Ludicard <luckydicey38@gmail.com>
2025-04-30 20:09:29 -0500

    Add project files.

---
+++ TallerMVC2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TallerMVC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
